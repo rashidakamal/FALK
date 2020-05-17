ae10d5acc83233628e9db25b421fe94735ff7dbe
Rashida Kamal <rsk2161@columbia.edu>
2020-05-17 11:33:53 -0400

    more notes

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ It's a dream.
 
 Personal set of tools for kitchen, pantry, and recipe management. 
 
+I'm using this as an experiment to help me cook yummy recipes from my at-home cookbooks and a few online blogs I follow, as well as be more conscious of meal planning and prepping as a way of reducing food waste in my household. 
+
+I will make no attempt to automate the cooking itself – that is the fun part! This projects exists solely to get me to the fun part with greater ease. 
+
+On the technical side, I will be practicing working with React and building my own React components.
--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const express = require('express'); 
 const bodyParser = require('body-parser');
-const cors = require('cors'); 
 
 const app = express();
 const port = 3005;
 
-app.use(cors);
+let recipes = [];
+
 
 // middleware 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json()); 
 
 app.get('/', (req, res) => {
-	res.send('TESTTEST');
+	res.send('TESTTESTnow');
+});
+
+app.post('/recipes', (req, res) => {
+
+	// recipe text to thus URI your databases here (form action? maybe.)
+	const recipe = req.body;
+	
 });
 
 app.listen(port, () => console.log('is your kitchen automatic? no. good. ${port} !')); 
@@ -21,3 +28,4 @@ app.listen(port, () => console.log('is your kitchen automatic? no. good. ${port}
 
 // CODING RESOURCES / SOURCES:
 // setting up basics: https://stackabuse.com/building-a-rest-api-with-node-and-express/
+
